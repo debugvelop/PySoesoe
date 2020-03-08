f1a9a8cf4c9bbe20344fbc702bba2188d511e15d
Debug Develop <debugvelop>
2020-03-08 14:39:30 +0700

    fixing formula

--- Numerical/Simpson_Integration.py
+++ Numerical/Simpson_Integration.py
@@ -2,16 +2,16 @@ import math
 #iteration number must be even
 def function(input):
     #current function = sin(x) to the power of 0.5
-    return math.sin(pow(input, 0.50000))
+    return math.sin(math.sqrt(input))
 def simpsonIntegration(upper, lower, iteration):
     evens_Sum = 0
     odds_Sum = 0
-    deltaX  = (upper - lower) / (iteration * 3.00000)
+    deltaX  = (upper - lower) / iteration
     for even in range(2, iteration, 2):
         evens_Sum = function(lower + (even * deltaX)) + evens_Sum
     for odd in range(3, iteration - 1, 2):
         odds_Sum  = function(lower + (odd * deltaX)) + odds_Sum
-    return deltaX * (function(lower) + (2 * evens_Sum) + (4 * odds_Sum) + function(upper))
+    return (deltaX / 3.00000) * (function(lower) + (2.00000 * evens_Sum) + (4.00000 * odds_Sum) + function(upper))
 upper = int(input("Upper Limit: "))
 lower = int(input("Lower Limit: "))
 iteration = int(input("Iteration: "))
