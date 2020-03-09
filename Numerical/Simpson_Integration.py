e30bc4d9f29d682417ac91f96297c9031816e822
Debug Develop <debugvelop>
2020-03-09 17:41:43 +0700

    make it accepts various functions through sympy

--- Numerical/Simpson_Integration.py
+++ Numerical/Simpson_Integration.py
@@ -1,18 +1,23 @@
-import math
-#iteration number must be even
+from sympy import sympify, symbols
+x = symbols("x")
+
 def function(input):
-    #current function = sin(x) to the power of 0.5
-    return math.sin(math.sqrt(input))
+    return func.subs(x, input)
+
+#iteration number must be even
 def simpsonIntegration(upper, lower, iteration):
     evens_Sum = 0
     odds_Sum = 0
     deltaX  = (upper - lower) / iteration
     for even in range(2, iteration, 2):
-        evens_Sum = function(lower + (even * deltaX)) + evens_Sum
+        evens_Sum += function(lower + (even * deltaX))
     for odd in range(3, iteration - 1, 2):
-        odds_Sum  = function(lower + (odd * deltaX)) + odds_Sum
+        odds_Sum  += function(lower + (odd * deltaX))
     return (deltaX / 3.00000) * (function(lower) + (2.00000 * evens_Sum) + (4.00000 * odds_Sum) + function(upper))
-upper = int(input("Upper Limit: "))
-lower = int(input("Lower Limit: "))
+
+expression = input("Function: ")
+upper = float(input("Upper Limit: "))
+lower = float(input("Lower Limit: "))
 iteration = int(input("Iteration: "))
+func = sympify(expression)
 print(simpsonIntegration(upper, lower, iteration))
