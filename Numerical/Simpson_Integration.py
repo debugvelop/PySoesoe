5879323197423a6fcd37c20642224e724882f92f
Debug Develop <debugvelop>
2020-03-12 08:26:32 +0700

    fixing formula

--- Numerical/Simpson_Integration.py
+++ Numerical/Simpson_Integration.py
@@ -9,11 +9,11 @@ def simpsonIntegration(upper, lower, iteration):
     evens_Sum = 0
     odds_Sum = 0
     deltaX  = (upper - lower) / iteration
-    for even in range(2, iteration, 2):
+    for even in range(1, iteration, 2):
         evens_Sum += function(lower + (even * deltaX))
-    for odd in range(3, iteration - 1, 2):
+    for odd in range(2, iteration - 1, 2):
         odds_Sum  += function(lower + (odd * deltaX))
-    return (deltaX / 3.00000) * (function(lower) + (2.00000 * evens_Sum) + (4.00000 * odds_Sum) + function(upper))
+    return (deltaX / 3) * (function(lower) + (4 * evens_Sum) + (2 * odds_Sum) + function(upper))
 
 expression = input("Function: ")
 upper = float(input("Upper Limit: "))
