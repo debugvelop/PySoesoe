26d4a0a2a61237191720df298cc63aca5e776775
Debug Develop <debugvelop>
2020-03-12 09:05:56 +0700

    reverting formula, fixing interval

--- Numerical/Simpson_Integration.py
+++ Numerical/Simpson_Integration.py
@@ -13,7 +13,7 @@ def simpsonIntegration(upper, lower, iteration):
         evens_Sum += function(lower + (even * deltaX))
     for odd in range(2, iteration - 1, 2):
         odds_Sum  += function(lower + (odd * deltaX))
-    return (deltaX / 3) * (function(lower) + (4 * evens_Sum) + (2 * odds_Sum) + function(upper))
+    return (deltaX / 3) * (function(lower) + (2 * evens_Sum) + (4 * odds_Sum) + function(upper))
 
 expression = input("Function: ")
 upper = float(input("Upper Limit: "))
